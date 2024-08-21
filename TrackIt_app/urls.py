3ffe9444e44367434b913bfc453e9690a9657b08
Khyle Andrei B. Colarina <khylebcolarina@gmail.com>
2024-08-21 14:46:11 +0800

    (Sign-Up, Log-In and Admin-LogIn) Committed

---
+++ TrackIt_app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/',views.user_login, name='user_login'),
+    path('signup/',views.user_signup, name='user_signup'),
+    path('admin-login/',views.sysAdLogin, name='sysAdLogin'),
+]
--- TrackIt_app/views.py
+++ TrackIt_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_login(request):
+    return render(request, "user_login.html")
+
+def user_signup(request):
+    return render(request, "user_signup.html")
+
+def sysAdLogin(request):
+    return render(request, "system_admin/sysAdLogin.html")
+
