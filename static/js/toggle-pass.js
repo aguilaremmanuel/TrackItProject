96a44d0dcd91e2fa27077ae2afcac8c63354ff07
Khyle Andrei B. Colarina <khylebcolarina@gmail.com>
2024-08-28 21:45:59 +0800

    Disable Role Admin
    Role Base na yung pag select

--- static/js/toggle-pass.js
+++ static/js/toggle-pass.js
@@ -1,27 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
     const togglePassword = document.querySelector('#togglePassword');
     const password = document.querySelector('#password');
-    
     const togglePassword2 = document.querySelector('#togglePassword2');
     const password2 = document.querySelector('#password2');
 
+    const departmentSelect = document.querySelector('#selectDept');
+    const roleSelect = document.querySelector('#selectRole');
+
     togglePassword.addEventListener('click', function () {
-        // Toggle the type attribute for the first password field
         const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
         password.setAttribute('type', type);
-
-        // Toggle the eye icon
         this.classList.toggle('fa-eye');
         this.classList.toggle('fa-eye-slash');
     });
 
     togglePassword2.addEventListener('click', function () {
-        // Toggle the type attribute for the second password field
         const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
         password2.setAttribute('type', type);
-
-        // Toggle the eye icon
         this.classList.toggle('fa-eye');
         this.classList.toggle('fa-eye-slash');
+    });
+
+    departmentSelect.addEventListener('change', function () {
+        const selectedDepartment = departmentSelect.value;
+
+        if (selectedDepartment === 'ADM') {
+            roleSelect.value = 'ADO';
+            roleSelect.setAttribute('disabled', 'disabled');
+        } else {
+            roleSelect.removeAttribute('disabled');
+            const restrictedRoles = ['ADO'];  // Restricted roles for certain departments
+            if (['ACC', 'BMD', 'CSR', 'PRL'].includes(selectedDepartment)) {
+                restrictedRoles.forEach(role => {
+                    const option = roleSelect.querySelector(`option[value="${role}"]`);
+                    if (option) {
+                        option.setAttribute('disabled', 'disabled');
+                    }
+                });
+            } else {
+                restrictedRoles.forEach(role => {
+                    const option = roleSelect.querySelector(`option[value="${role}"]`);
+                    if (option) {
+                        option.removeAttribute('disabled');
+                    }
+                });
+            }
+        }
     });
 });
