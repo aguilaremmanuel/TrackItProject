8b1ceed965616ae3eccd9155364c1132373077f7
Khyle Andrei B. Colarina <khylebcolarina@gmail.com>
2024-09-21 12:32:35 +0800

    Update QR for System AD & Director

--- TrackIt_app/urls.py
+++ TrackIt_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import generate_qr_code
 
 urlpatterns = [
     path('login/',views.user_login, name='user_login'),
