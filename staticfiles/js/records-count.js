d793d45312d639c1172ab525b5fd3dcafac5df6f
Emmanuel John Aguilar <aguilaremman3@gmail.com>
2024-11-03 22:24:46 +0800

    delete static files

--- staticfiles/js/records-count.js
+++
@@ -1,6 +0,0 @@
-const tableBodies = document.querySelectorAll('.tab-pane.active tbody');
-tableBodies.forEach((tbody) => {
-    const recordCountElement = document.getElementById('record-count');
-    const rowCount = tbody.querySelectorAll('tr').length;
-    recordCountElement.textContent = `Showing total of ${rowCount} record(s)`;
-});
